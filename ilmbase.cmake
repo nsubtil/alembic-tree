c6aec513d15fa9275eafcb9fd2384f9403b88841
Nuno Subtil <subtil@gmail.com>
2016-04-09 17:35:39 -0700

    Builds Alembic + dependencies

---
+++ ilmbase.cmake
@@ -0,0 +1,27 @@
+set(openexr_SOURCE ${CMAKE_SOURCE_DIR}/openexr)
+set(ilmbase_INSTALL ${CMAKE_BINARY_DIR}/install)
+
+# install ilmbase from openexr
+ExternalProject_Add(ilmbase
+                    DOWNLOAD_COMMAND ""
+                    SOURCE_DIR ${openexr_SOURCE}
+                    INSTALL_DIR ${ilmbase_INSTALL}
+                    CONFIGURE_COMMAND
+                        ${CMAKE_COMMAND}
+                        -DCMAKE_INSTALL_PREFIX=${ilmbase_INSTALL}
+                        ${openexr_SOURCE}/IlmBase
+                    LOG_DOWNLOAD 1
+                    LOG_INSTALL 1)
+
+# ExternalProject_Add(pyilmbase
+#                     DEPENDS ilmbase
+#                     DOWNLOAD_COMMAND ""
+#                     SOURCE_DIR ${openexr_SOURCE}
+#                     INSTALL_DIR ${ilmbase_INSTALL}
+#                     CONFIGURE_COMMAND
+#                         env ILMBASE_PACKAGE_PREFIX=${ilmbase_INSTALL}
+#                         ${CMAKE_COMMAND}
+#                         -DCMAKE_INSTALL_PREFIX=${ilmbase_INSTALL}
+#                         ${openexr_SOURCE}/PyIlmBase
+#                     LOG_DOWNLOAD 1
+#                     LOG_INSTALL 1)
